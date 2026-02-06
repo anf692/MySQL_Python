03e76822865b0f8c3e76b773d7039491eded02d9
anf692 <fassane348@gmail.com>
2026-02-06 11:43:56 +0000

    ajout de la fonction pour ajouter les apprenants (ajout_aprenant)

--- main.py
+++ main.py
@@ -9,3 +9,15 @@ connexion = mysql.connector.connect(
 )
 
 curseur = connexion.cursor()
+
+
+def ajouter_apprenant():
+    nom = input("Nom : ")
+    prenom = input("Prénom : ")
+    promo = input("Promo : ")
+    curseur.execute(
+        "INSERT INTO apprenants (nom, prenom, promo) VALUES (%s, %s, %s)",
+        (nom, prenom, promo)
+    )
+    connexion.commit()
+    print(f"Apprenant {nom} {prenom} ajouté avec succès.")
